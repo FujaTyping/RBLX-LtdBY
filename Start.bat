31938a24dd267aaaec08566e616db773ba1217e9
Siraphop Sukchu <soraphopsukchu@gmail.com>
2023-05-01 19:50:21 +0700

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,3 +1,12 @@
 @echo off
+echo Please install python before run this file
+where python >nul 2>nul
+if %errorlevel% equ 0 (
+    echo Detect Python is on this system
+) else (
+    echo Detect Python is not on this system - Download python from : ^
+https://www.python.org/downloads/
+)
+echo ------------------------------------------
 python Main.py
 pause
