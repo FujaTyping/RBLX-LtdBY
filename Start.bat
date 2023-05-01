7af97552fd499e4550a939e9bd240ddfd7a48e2c
Siraphop Sukchu <soraphopsukchu@gmail.com>
2023-05-01 22:28:45 +0700

    Update file

--- Start.bat
+++ Start.bat
@@ -2,7 +2,20 @@
 echo Please install python before run this file
 where python >nul 2>nul
 if %errorlevel% equ 0 (
+    setlocal enabledelayedexpansion
     echo Detect Python is on this system
+
+    set "Req="
+    set /p Req=Do you want to install all package y/N : 
+
+    if "!Req!"=="y" (
+        echo Installing all package file
+        pip install requests
+        pip install uuid
+        echo All package has been install
+    )
+
+    endlocal
 ) else (
     echo Detect Python is not on this system - Download python from : ^
 https://www.python.org/downloads/
